12143740cd5ca97c4801f2ca6d2ae003c4ce2530
Hunter3592 <90448429+Hunter3592@users.noreply.github.com>
2021-11-15 11:11:01 +0100

    EEHHEHE

---
+++ rescoures/js/js innlecering.js
@@ -0,0 +1,17 @@
+// Javascript document*//
+
+
+image_Array = [
+ 'funny doge.jpg',
+ 'amogus1.jpg',
+ 'amogus2.jpg',
+ 'amogus3.jpg',
+ 'amogus4.jpg'
+	
+]
+
+function getrandomimage() {
+	
+	
+	
+}
